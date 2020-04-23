8749c8cbd1608d28ab45bbd1c383f948f12f0487
jamiehadd <jhaddock@ucdavis.edu>
2020-04-22 21:10:39 -0700

    Initial commit

---
+++ src/ssnmf/__init__.py
@@ -0,0 +1,14 @@
+"""Top-level package for SSNMF."""
+
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.0.1"
+
+__author__ = "Jamie Haddock"
+__email__ = "jhaddock@math.ucla.edu"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2020, Jamie Haddock"
+
+
+from .example import Example  # noqa: F401
---
+++ src/ssnmf/example.py
@@ -0,0 +1,12 @@
+class Example:
+    """This class is only an example."""
+
+    def get_value(self):
+        """Spoiler: it's 10.
+
+        Example:
+            >>> e = Example()
+            >>> e.get_value()
+            10
+        """
+        return 10
