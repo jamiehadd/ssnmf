d9c3c09eae2a85158f13dac277e13a39cd96f4bb
jamiehadd <jhaddock@ucdavis.edu>
2020-05-14 12:45:22 -0700

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -83,6 +83,6 @@ setup(
     url="https://github.com/jamiehadd/ssnmf",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.0.1",
+    version="0.0.2",
     zip_safe=False,
 )
--- src/ssnmf/__init__.py
+++ src/ssnmf/__init__.py
@@ -2,7 +2,7 @@
 
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 __author__ = "Jamie Haddock"
 __email__ = "jhaddock@math.ucla.edu"
