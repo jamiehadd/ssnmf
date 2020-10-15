164b539cf0873936234b24ba67f1888dcecf70d0
Jamie Haddock <jamie.lynn.haddock@gmail.com>
2020-10-15 08:15:23 -0700

    Bump version: 1.0.2 → 1.0.3

--- src/ssnmf/__init__.py
+++ src/ssnmf/__init__.py
@@ -1,6 +1,6 @@
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 __author__ = "Jamie Haddock"
 __email__ = "jhaddock@math.ucla.edu"
